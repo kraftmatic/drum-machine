fca14349bb9be5ee127e6e5f17648fd11b18b875
Nicholas Kraftor <nicholas.p.kraftor@ehi.com>
2014-12-07 23:08:58 -0600

    Add BPM adjustment to metronome.  Add stop button.  Implement layout for drum machine.

--- Drum Machine/MetronomeViewController.swift
+++ Drum Machine/MetronomeViewController.swift
@@ -12,8 +12,13 @@ import AVFoundation
 
 class MetronomeViewController: UIViewController, MetronomeDelegate {
     
+    @IBOutlet weak var bpmLabel: UILabel!
+    @IBOutlet weak var bpmField: UITextField!
+    @IBOutlet weak var startButton: UIButton!
     var metronome: MetronomeModel = MetronomeModel()
     var audioPlayer = AVAudioPlayer()
+    
+    var metronomeStarted: Bool = false;
     
     override init (){
         super.init()
@@ -34,12 +39,26 @@ class MetronomeViewController: UIViewController, MetronomeDelegate {
     }
 
     func metronomeTick() {
-        println("test in controller")
         audioPlayer.play()
         
     }
     
     @IBAction func startButtonPressed(sender: UIButton) {
-        self.metronome.startMetronome()
+        if metronomeStarted == false {
+            
+            var bpm = self.bpmField.text.toInt()
+            bpmLabel.text = self.bpmField.text + " BPM"
+            metronome.startMetronome(bpm!)
+            startButton.setTitle("Stop", forState: UIControlState.Normal)
+            metronomeStarted = true
+            
+        } else {
+            
+            metronome.stopMetronome()
+            startButton.setTitle("Start", forState: UIControlState.Normal)
+            bpmLabel.text = "Stopped"
+            metronomeStarted = false
+            
+        }
     }
 }
